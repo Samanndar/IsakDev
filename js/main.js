69aeaf6600339adbf81f6c57f3f1fe62633e50f6
Samandar <samandartursunov217@gmail.com>
2024-02-14 12:11:06 +0500

    Add class updated header__flex postion and header styles burger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 function init () {
-  
+  // BURGER
+  const isIcon = document.querySelector(".header__icon");
+
+  if(isIcon) {
+    const isNav = document.querySelector(".header__nav")
+    isIcon.addEventListener("click", function() {
+      isIcon.classList.toggle("active")
+      isNav.classList.toggle("active")
+      document.body.classList.toggle("lock")
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', init);
