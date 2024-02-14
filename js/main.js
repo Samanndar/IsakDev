7ee23feda4f76f067c2cbec5af2b06dca74389b9
Samandar <samandartursunov217@gmail.com>
2024-02-14 19:19:03 +0500

    Update js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 function init () {
   // BURGER
   const isIcon = document.querySelector(".header__icon");
+  const isNav = document.querySelector(".header__nav");
 
   if(isIcon) {
-    const isNav = document.querySelector(".header__nav")
     isIcon.addEventListener("click", function() {
       isIcon.classList.toggle("active")
       isNav.classList.toggle("active")
       document.body.classList.toggle("lock")
     })
   }
+  
   // ACTIVE-MENU
   const actItem = document.querySelectorAll(".nav__link");
   actItem.forEach(item => {
     item.addEventListener("click", function(e) {
       actItem.forEach(act => {act.classList.remove("nav__link--active")})
       item.classList.toggle("nav__link--active")
+      if(isNav.classList.contains("active")) {
+        isIcon.classList.remove("active")
+        isNav.classList.remove("active")
+        document.body.classList.remove("lock")
+      }
     })
   })
 }
